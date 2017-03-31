4ca0dde4b35bcc9d4b6eaa25992131385d046c87
Adrian Lopez <adrijardi@gmail.com>
2017-03-31 08:44:19 +0100

    Almost end chapter 5

--- src/main/scala/Chapter5.scala
+++ src/main/scala/Chapter5.scala
@@ -1,3 +1,7 @@
+import Chapter5.Stream.{cons, empty, unfold}
+
+import scala.annotation.tailrec
+
 /**
   * Functional programming in Scala, exercises chapter 5
   */
@@ -20,7 +24,7 @@ object Chapter5 {
     /**
       * Exercise 5.2
       */
-    def take(n: Int): Stream[A] = {
+    def take(n: Int): Stream[A] =
       if(n <= 0) {
         Empty
       } else {
@@ -29,17 +33,106 @@ object Chapter5 {
           case Cons(h, t) => Cons(h, () => t().take(n-1))
         }
       }
-    }
 
-    def drop(n: Int): Stream[A] = {
+    def drop(n: Int): Stream[A] =
       if(n <= 0) {
         this
       } else {
         this match {
           case Empty => Empty
-          case Cons(h, t) => t().drop(n-1)
+          case Cons(_, t) => t().drop(n-1)
         }
       }
+
+    /**
+      * Exercise 5.3
+      */
+    def takeWhile(p: A => Boolean): Stream[A] = this match {
+      case Empty => Empty
+      case Cons(h, t) =>
+        if(p(h())) {
+          Cons(h, () => t().takeWhile(p))
+        } else {
+          Empty
+        }
+    }
+
+    /**
+      * Exercise 5.4
+      */
+    @tailrec
+    final def forAll(p: A => Boolean): Boolean = this match {
+      case Empty => true
+      case Cons(h, t) => p(h()) && t().forAll(p)
+    }
+
+    /**
+      * Exercise 5.5
+      */
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
+      this match {
+        case Cons(h,t) => f(h(), t().foldRight(z)(f))
+        case _ => z
+      }
+
+    def takeWhile2(p: A => Boolean): Stream[A] = foldRight(empty[A])((e, r) =>
+      if(p(e)) {
+        cons(e, r)
+      } else {
+        Empty
+      }
+    )
+
+    /**
+      * Exercise 5.6
+      */
+    def headOption: Option[A] = foldRight[Option[A]](None)( (e, _) => return Some(e) )
+
+    /**
+      * Exercise 5.7
+      */
+    def map[B](f: A => B): Stream[B] = foldRight(empty[B])((e, r) => cons(f(e), r) )
+
+    def filter(f: A => Boolean): Stream[A] = foldRight(empty[A])((e, r) =>
+      if(f(e)) {
+        cons(e, r)
+      } else {
+        r
+      }
+    )
+
+    def append[AA >: A](a: Stream[AA]): Stream[AA] = foldRight(a)( (e, r) => cons(e, r) )
+
+    def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((e, r) => f(e).append(r))
+
+    /**
+      * Exercise 5.13
+      */
+    def map2[B](f: A => B): Stream[B] = unfold(this) {
+      case Empty => None
+      case Cons(h, t) => Some((f(h()), t()))
+    }
+
+    def take2(n: Int): Stream[A] = unfold((this, n)) {
+      case (Cons(h, t), n1) if n1 > 0 => Some((h(), (t(), n1-1)))
+      case _ => None
+    }
+
+    def takeWhile3(p: A => Boolean): Stream[A] = unfold(this) {
+      case Cons(h, t) if p(h()) => Some((h(), t()))
+      case _ => None
+    }
+
+    def zipWith[B,C](b: Stream[B])(f: (A,B) => C): Stream[C] = unfold((this, b)) {
+      case (Cons(h1,t1), Cons(h2,t2)) => Some(( f(h1(), h2()), (t1(), t2()) ))
+      case _ => None
+    }
+
+    def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = unfold((this, s2)) {
+      case (Cons(h1,t1), Cons(h2,t2)) => Some( (Some(h1()), Some(h2())), (t1(), t2()) )
+      case (Cons(h1,t1), _) => Some( (Some(h1()), None), (t1(), Empty) )
+      case (_, Cons(h2,t2)) => Some( (None, Some(h2())), (Empty, t2()))
+      case _ => None
     }
   }
 
@@ -59,6 +152,45 @@ object Chapter5 {
 
     def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
+    /**
+      * Exercise 5.8
+      */
+    def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+    /**
+      * Exercise 5.9
+      */
+    def from(n: Int): Stream[Int] = cons(n, from(n+1))
+
+    /**
+      * Exercise 5.10
+      */
+    def fibs: Stream[Long] = {
+      def go(a: Long, b: Long): Stream[Long] = {
+        cons(a+b, go(b, a+b))
+      }
+
+      cons(0, cons(1, go(0, 1)))
+    }
+
+    /**
+      * Exercise 5.11
+      */
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+      case None => Empty
+      case Some((a, s)) => cons(a, unfold(s)(f))
+    }
+
+    /**
+      * Exercise 5.12
+      */
+    def fibs2: Stream[Long] = cons(0, cons(1L, unfold((0L,1L)){ case (a,b) => Some((a+b, (b, a+b))) } ))
+
+    def from2(n: Int): Stream[Int] = unfold(n)( v => Some(v, v+1) )
+
+    def constant2[A](a: A): Stream[A] = unfold(())(_ => Some(a, ()))
+
+    def ones2: Stream[Int] = unfold(())(_ => Some(1, ()))
   }
 
 }
--- testChapter5.sc
+++ testChapter5.sc
@@ -1,18 +1,55 @@
 import Chapter5._
 import Stream._
 
-def infinite(v: Int): Stream[Int] = cons(v, infinite(v*3))
-
-def finite(v: Int): Stream[Int] =
-  if(v < 10000){
-    cons(v, finite(v*3))
+def gen[A](s: A, f: (A) => A, doWhile: A => Boolean): Stream[A] =
+  if(doWhile(s)){
+    cons(s, gen(f(s), f, doWhile))
   } else {
     Empty
   }
 
-finite(3).toList
+def infinite(s: Int, f: (Int) => Int): Stream[Int] = gen[Int](s, f, _ => true)
+val infinite3x3 = infinite(3, _ * 3)
 
-infinite(3).take(10).toList
-infinite(3).drop(10).take(10).toList
+def finite(s: Int, f: (Int) => Int, max: Int = 10000): Stream[Int] = gen[Int](s, f, _ < max)
 
 
+finite(3, _ * 3).toList
+
+infinite3x3.take(10).toList
+infinite3x3.drop(10).take(10).toList
+
+
+infinite3x3.takeWhile(_ < 10000).toList
+
+infinite3x3.forAll(_ < 100000)
+
+infinite3x3.takeWhile2(_ < 10000).toList
+
+infinite3x3.headOption
+
+infinite(1, _+1).map(_+1).take(10).toList
+finite(1, _ + 1).map(_+1).toList
+
+finite(1, _ + 1).filter(_ % 2 == 0).toList
+
+gen[Int](1, _+1, _ < 10).append(Stream.cons(2000, Empty)).toList
+
+gen[Int](1, _+1, _ < 10).flatMap(v => Stream.cons(v, Stream.cons(v+10, Empty))).toList
+
+constant(6).take(100).toList
+from(6).take(100).toList
+
+fibs.take(100).toList
+
+fibs2.take(100).toList
+constant2(6).take(100).toList
+from2(6).take(100).toList
+ones2.take(100).toList
+
+
+infinite(1, _+1).map2(_+1).take(10).toList
+infinite3x3.take2(10).toList
+infinite3x3.takeWhile3(_ < 10000).toList
+infinite3x3.zipWith(finite(1, _ + 2, 10))(_+_).toList
+finite(1, _ + 2, 20).zipAll(finite(1, _ + 1, 20)).toList
