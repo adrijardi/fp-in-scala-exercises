f3db9180ef753c37d1db88c08f37399872f943e2
adrijardi@gmail.com <adrian.lopez@adstream.com>
2017-04-04 19:41:28 +0100

    Chapter 6 completed

--- src/main/scala/Chapter6.scala
+++ src/main/scala/Chapter6.scala
@@ -1,3 +1,5 @@
+import Chapter6.State.{get, modify, set}
+
 /**
   * Functional programming in Scala, exercises chapter 6
   */
@@ -71,6 +73,131 @@ object Chapter6 {
       }
 
     go(count, Nil, rng)
+  }
+
+  /**
+    * Exercise 6.5
+    */
+  type Rand[+A] = RNG => (A, RNG)
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def double_ : Rand[Double] =
+    map[Int, Double](nonNegativeInt) { rnd =>
+      math.min(rnd, Int.MaxValue - 1).toDouble / Int.MaxValue
+    }
+
+  /**
+    * Exercise 6.6
+    */
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, newNrg) = ra(rng)
+    val (b, endNrg) = rb(newNrg)
+    ( f(a,b), endNrg)
+  }
+
+  /**
+    * Exercise 6.7
+    */
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = rng => fs match {
+    case Nil => (Nil, rng)
+    case h :: t =>
+      val (l, newNrg) = h(rng)
+      val r: (List[A], RNG) = sequence(t)(newNrg)
+      (l :: r._1, r._2)
+  }
+
+  /**
+    * Exercise 6.8
+    */
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, newRng) = f(rng)
+    g(a)(newRng)
+  }
+
+  /**
+    * Exercise 6.9
+    */
+  def map_[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+    val lift: A => Rand[B] = a => rng => (f(a), rng)
+    flatMap(s)(lift)
+  }
+
+  def map2_[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)( a => map(rb) (b => f(a, b)))
+
+  /**
+    * Exercise 6.10
+    */
+  case class State[S,+A](run: S => (A,S)) {
+    def map[B](f: A => B): State[S,B] = flatMap(f.andThen(State.unit))
+
+    def flatMap[B](f: A => State[S,B]): State[S,B] = State{
+      s: S => {
+        val (a, s1) = run(s)
+        f(a).run(s1)
+      }
+    }
+
+  }
+
+  object State {
+    def unit[S,A](a: A): State[S,A] = State(s => (a, s))
+
+    def map2[S,A,B,C](sa: State[S,A], sb: State[S,B])(f: (A, B) => C): State[S,C] =
+      sa.flatMap(a => sb.map( f(a,_) ))
+
+    def sequence[S,A](fs: List[State[S,A]]): State[S,List[A]] = State { si: S =>
+
+      fs.foldLeft( (List.empty[A], si) )( (res, e) => {
+        val (a, newS): (A, S) = e.run(res._2)
+        (a :: res._1, newS)
+      })
+    }
+
+    def get[S]: State[S, S] = State(s => (s, s))
+
+    def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+    def modify[S](f: S => S): State[S, Unit] = for {
+      s <- get
+      _ <- set(f(s))
+    } yield ()
+  }
+
+  /**
+    * Exercise 6.11
+    */
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+  val insertCoin: State[Machine, Unit] = modify {
+    case Machine(true, candies, coins) if candies > 0 => Machine(false, candies, coins +1)
+    case s => s
+  }
+
+  val turnKnob: State[Machine, Unit] = modify {
+    case Machine(false, candies, coins) => Machine(true, candies -1, coins)
+    case o => o
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+
+    val modifications: List[State[Machine, Unit]] = inputs.map {
+      case Coin => insertCoin
+      case Turn => turnKnob
+    }
+
+    for {
+      _ <- State.sequence(modifications)
+      s <- get
+    } yield (s.coins, s.candies)
   }
 
 }
--- testChapter6.sc
+++ testChapter6.sc
@@ -12,3 +12,20 @@ val (res5, rng5) = double3(rng4)
 
 val (res6, rng6) = ints(6)(rng5)
 
+val (res7, rng7) = double_(rng6)
+
+val (res8, rng8) = map2(ints(6), double_)( (a,b) => a.map(_*b) )(rng7)
+
+val (res9, rng9) = sequence(List(double_, double_, nonNegativeInt(_)))(rng8)
+
+val (res10, rng10) = flatMap(double_)(d => ints((d*10).toInt)(_))(rng9)
+
+double_(rng7)._1 * 3
+val (res11, rng11) = map_(double_)( _ * 3)(rng7)
+
+val (res12, rng12) = map2_(ints(6), double_)( (a,b) => a.map(_*b) )(rng7)
+
+val inputs = List(Coin, Turn, Coin, Turn)
+val initialState: Machine = Machine(true, 10, 2)
+
+val ( (coins, candies), machine) = simulateMachine(inputs).run(initialState)
