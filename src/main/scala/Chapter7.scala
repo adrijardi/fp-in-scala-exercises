5cfd366a5c1cf93ffd20de20fc811b2da7e77e98
Adrian Lopez <adrijardi@gmail.com>
2017-04-05 23:18:19 +0100

    Going cucu with the low level calls

--- src/main/scala/Chapter7.scala
+++ src/main/scala/Chapter7.scala
@@ -1,9 +1,13 @@
+import java.util.concurrent.ExecutorService
+
 /**
   * Functional programming in Scala, exercises chapter 7
   */
 object Chapter7 {
 
-  case class Par[+A](a: () => A)
+  sealed trait Par[+A]
+  case class SyncPar[+A](a: A) extends Par[A]
+  case class AsyncPar[+A](a: () => A) extends Par[A]
 
   /**
     * Exercise 7.2
@@ -14,11 +18,22 @@ object Chapter7 {
     * Exercise 7.2
     */
   object Par {
-    def unit[A](a: A): Par[A] = ???
-    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
-    def fork[A](a: => Par[A]): Par[A] = ???
+    val es: ExecutorService
+
+    def unit[A](a: A): Par[A] = SyncPar(a)
+
+    def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = lazyUnit(f(run(a), run(b)))
+
+    def fork[A](a: => Par[A]): Par[A] = AsyncPar(() => a)
+
     def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-    def run[A](a: Par[A]): A = ???
+
+    def run[A](a: Par[A]): A = a match {
+      case SyncPar(v) => v
+      case AsyncPar(v) =>
+        es.submit(v)
+    }
+
   }
 
 }
